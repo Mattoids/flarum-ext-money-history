85becd1b787efb18dbefd0f17ce4ea0bbb024b52
liufei <liufei@liufei.info>
2023-12-12 15:08:56 +0800

    删除不必要的类

--- src/Listeners/HistoryListeners.php
+++ src/Listeners/HistoryListeners.php
@@ -14,13 +14,14 @@ abstract class HistoryListeners
     public function exec(?User $user, $money) {
 
         if ($money > 0 || $money < 0) {
+            $money = $money > 0 ? $money : -$money;
             $userMoneyHistory = new UserMoneyHistory();
             $userMoneyHistory->user_id = $user->id;
             $userMoneyHistory->type = $money > 0 ? "C" : "D";
-            $userMoneyHistory->money = $money > 0 ? $money : -$money;
+            $userMoneyHistory->money = $money;
             $userMoneyHistory->source = $this->source;
             $userMoneyHistory->source_desc = $this->sourceDesc;
-            $userMoneyHistory->balance_money = 0;
+            $userMoneyHistory->balance_money = isset($user->init_money) ? $user->init_money : $user->money - $money;
             $userMoneyHistory->last_money = $user->money;
             $userMoneyHistory->create_user_id = isset($user->create_user_id) ? $user->create_user_id : $user->id;
             $userMoneyHistory->change_time = Date("Y-m-d H:i:s");
