d93ebb1efcbecaa5b26f0ce8a15a482e1075652a
liufei <liufei@liufei.info>
2023-12-12 13:50:22 +0800

    删除操作日志相关逻辑

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,33 +1,5 @@
 import app from 'flarum/admin/app';
 
-app.initializers.add('mattoid-money-history', () => {
-  app.extensionData.for("mattoid-money-history")
-    .registerSetting({
-      setting: 'mattoid-money-history.request-type-get',
-      help: app.translator.trans('mattoid-money-history.admin.settings.request-type-get-requirement'),
-      label: app.translator.trans('mattoid-money-history.admin.settings.request-type-get'),
-      type: 'switch',
-      default: false
-    })
-    .registerSetting({
-      setting: 'mattoid-money-history.request-type-post',
-      help: app.translator.trans('mattoid-money-history.admin.settings.type-requirement'),
-      label: app.translator.trans('mattoid-money-history.admin.settings.request-type-post'),
-      type: 'switch',
-      default: true
-    })
-    .registerSetting({
-      setting: 'mattoid-money-history.request-type-put',
-      help: app.translator.trans('mattoid-money-history.admin.settings.type-requirement'),
-      label: app.translator.trans('mattoid-money-history.admin.settings.request-type-put'),
-      type: 'switch',
-      default: true
-    })
-    .registerSetting({
-      setting: 'mattoid-money-history.request-type-delete',
-      help: app.translator.trans('mattoid-money-history.admin.settings.type-requirement'),
-      label: app.translator.trans('mattoid-money-history.admin.settings.request-type-delete'),
-      type: 'switch',
-      default: true
-    })
+app.initializers.add('mattoid/flarum-ext-money-history', () => {
+  console.log('[mattoid/flarum-ext-money-history] Hello, forum and admin!');
 });
